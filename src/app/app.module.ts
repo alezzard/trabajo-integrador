080a62497ff5f500e948cf60d124d1ef1aceaf82
Ale-sentey <gabriel.alegodoy92@gmail.com>
2022-05-26 01:48:02 -0300

    alineando toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExpLabralComponent } from './components/exp-labral/exp-labral.component';
 import { ExpAcademicaComponent } from './components/exp-academica/exp-academica.component';
 import { ExpItemComponent } from './components/exp-item/exp-item.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,21 @@ import { ExpItemComponent } from './components/exp-item/exp-item.component';
     AcercaDeComponent,
     ExpLabralComponent,
     ExpAcademicaComponent,
-    ExpItemComponent
+    ExpItemComponent,
+    HabilidadesComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
